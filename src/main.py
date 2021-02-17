a0e6659106fa0bd1847ed65fb58389911c832c31
max kolomeychenko <maxim.kolomeychenko@gmail.com>
2021-02-17 17:39:05 +0200

    UI v - done

--- src/main.py
+++ src/main.py
@@ -3,17 +3,29 @@ import supervisely_lib as sly
 app: sly.AppService = sly.AppService()
 
 
-@app.callback("select_prev_object")
+@app.callback("create_foreground")
 @sly.timeit
-def prev_object(api: sly.Api, task_id, context, state, app_logger):
+def create_foreground(api: sly.Api, task_id, context, state, app_logger):
+    image_id = context["imageId"]
+    threshold = state["threshold"]
+    rewrite = state["rewrite"]
     pass
 
 
 def main():
-    data = {}
-    state = {}
+    data = {
+
+    }
+    state = {
+        "threshold": 255,
+        "rewrite": True,
+        "classes": ""
+    }
     app.run(data=data, state=state)
 
 
+# @TODO: size: mini - sly-select-class
+# @TODO: keep biggest connected component (ignore noise) VS keep all parts
+# @TODO: add min instance version to config
 if __name__ == "__main__":
     sly.main_wrapper("main", main)
