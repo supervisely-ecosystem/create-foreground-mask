1a1297fcf8b7d9bcba571531343b5ad5957be39f
max kolomeychenko <maxim.kolomeychenko@gmail.com>
2021-03-04 16:54:01 +0300

    works

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 import supervisely_lib as sly
 
 app = sly.AppService()
@@ -20,14 +21,14 @@ def create_classes(api: sly.Api):
     def _check_and_add_class(meta: sly.ProjectMeta, target_class: sly.ObjClass):
         cls: sly.ObjClass = meta.get_obj_class(target_class.name)
         if cls is None:
-            meta = meta.add_obj_class(foreground)
+            meta = meta.add_obj_class(target_class)
         elif cls != target_class:
             raise RuntimeError(f"Project already has class '{cls.name}' with shape {cls.geometry_type.geometry_name()}. "
                                f"Shape conflict: shape should be of typy {target_class.geometry_type.geometry_name()}")
         return meta
 
     global meta
-    meta_json = api.project.get_meta(PROJECT_ID)
+    meta_json = api.project.get_meta(project_id)
     meta = sly.ProjectMeta.from_json(meta_json)
 
     meta_res = _check_and_add_class(meta, foreground)
@@ -41,11 +42,14 @@ def create_classes(api: sly.Api):
 def get_masks(img):
     alpha = img[:, :, 3]
     mask_fg = alpha >= threshold
-    mask_fuzzy = alpha > 0 & alpha < 255
+    mask_fuzzy = np.logical_and(alpha > 0, alpha < 255)
 
     fg = sly.Label(sly.Bitmap(mask_fg), foreground)
-    fuzzy = sly.Label(sly.Bitmap(mask_fuzzy), fuzzy)
-    return fg, fuzzy
+    fzz = None
+    if np.any(mask_fuzzy) and need_fuzzy:
+        fzz = sly.Label(sly.Bitmap(mask_fuzzy), fuzzy)
+
+    return fg, fzz
 
 
 @app.callback("create_foreground")
@@ -71,26 +75,34 @@ def create_foreground(api: sly.Api, task_id, context, state, app_logger):
             anns_infos = api.annotation.download_batch(dataset.id, ids)
             anns = [sly.Annotation.from_json(info.annotation, meta) for info in anns_infos]
 
+            res_ids = []
             res_anns = []
             for img_id, img_name, img_path, ann in zip(ids, names, local_paths, anns):
-                img = sly.image.read(remove_alpha_channel=False)
+                img = sly.image.read(img_path, remove_alpha_channel=False)
                 if len(img.shape) == 3:
                     if img.shape[2] != 4:
                         sly.logger.warn(f"Image {img_name} (id={img_id}) does not have alpha channel, will be skipped")
+                        continue
                 else:
                     sly.logger.warn(f"Image {img_name} (id={img_id}) does not have alpha channel, will be skipped")
+                    continue
 
                 fg, fuzzy = get_masks(img)
-                to_add = [fg]
-                if need_fuzzy:
-                    to_add.append(fuzzy)
-                new_ann = ann.add_labels(to_add)
 
-                src_ids.append(img_id)
+                new_ann = ann.add_label(fg)
+                if fuzzy is not None:
+                    new_ann = new_ann.add_label(fuzzy)
+
+                res_ids.append(img_id)
                 res_anns.append(new_ann)
 
-            api.annotation.upload_anns(ids, res_anns)
+            api.annotation.upload_anns(res_ids, res_anns)
+            for img_path in local_paths:
+                sly.fs.silent_remove(img_path)
             progress.iters_done_report(len(batch_infos))
+
+    api.task.set_output_project(task_id, project_id)
+    app.stop()
 
 
 def main():
